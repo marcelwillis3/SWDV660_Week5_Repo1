b3ef4e5f16d5a97f80a92ed503c0dbc2c465a866
marcelwillis3 <56885255+marcelwillis3@users.noreply.github.com>
2021-02-14 19:11:47 -0500

    Update bitcoin_converter.py

--- bitcoin_converter.py
+++ bitcoin_converter.py
@@ -2,7 +2,7 @@
 # Author: Marcel Willis
 
 # Notify the user of the current bitcoin rate (source: Coinbase.com)
-print("As of 2/14/2021 at 7:10pm, bitcoin is currently trading at $48,767.60 per bitcoin")
+print("As of 2/14/2021 at 7:11pm, bitcoin is currently trading at $48,767.60 per bitcoin")
 
 # Allow user to input their number of bitcoins and convert to float
 bitcoin = float(input("Enter your bitcoin amount: "))
